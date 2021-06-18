f5863c70d526b16305cd1c09baa6d824a2e09210
Malbogle <josephjasson22@gmail.com>
2021-06-18 07:55:53 +0200

    changed  startup structure

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,7 @@ namespace Library
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Library v1"));
         }
 
-        app.UseHttpsRedirection();
+        //app.UseHttpsRedirection();
 
         app.UseRouting();
 
